ec3aca57c63d00207d199683a1d726644b28eb5d
Eduardo Fuentes <esteben305@gmail.com>
2022-03-14 11:11:50 -0500

    se añadieron animaciones a la seccion de monedas

--- js/index.js
+++ js/index.js
@@ -10,9 +10,11 @@ const coinTable2 = document.querySelector(".coins--section2")
 button1.addEventListener('click', () => {
     coinTable1.style.display = "none";
     coinTable2.style.display = "block";
+    coinTable2.style.animation = "0.5s section2 0s ease-in-out";
 });
 
 button2.addEventListener('click', () => {
     coinTable1.style.display = "block";
     coinTable2.style.display = "none";
+    coinTable1.style.animation = "0.5s section1 0s ease-in-out";
 });
