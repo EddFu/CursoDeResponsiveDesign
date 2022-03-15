c217dce71f24fd532ebdb089ed4e7b22ec99c894
Eduardo Fuentes <esteben305@gmail.com>
2022-03-15 09:32:27 -0500

    Se agrego el html de la seccion de detalles del producto

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 // vars //
 
-const button1 = document.querySelector("#arrow__button");
-const button2 = document.querySelector("#arrow__button2");
+const button1 = document.querySelector("#arrow__button--right");
+const button2 = document.querySelector("#arrow__button--left");
 
 const coinTable1 = document.querySelector(".coins--section")
 const coinTable2 = document.querySelector(".coins--section2")
@@ -10,11 +10,11 @@ const coinTable2 = document.querySelector(".coins--section2")
 button1.addEventListener('click', () => {
     coinTable1.style.display = "none";
     coinTable2.style.display = "block";
-    coinTable2.style.animation = "0.5s section2 0s ease-in-out";
+    coinTable2.style.animation = "0.5s section2 ease-in-out";
 });
 
 button2.addEventListener('click', () => {
     coinTable1.style.display = "block";
     coinTable2.style.display = "none";
-    coinTable1.style.animation = "0.5s section1 0s ease-in-out";
+    coinTable1.style.animation = "0.5s section1 ease-in-out";
 });
